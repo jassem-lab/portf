d8c0ea16a4036b9fd4ad3a6deec6098b899248b6
Jassem Gaaloul <56515129+jassem-lab@users.noreply.github.com>
2020-10-12 07:01:03 +0200

    Update app.js

--- app.js
+++ app.js
@@ -17,9 +17,9 @@ app.use("/api/works", require("./routes/works.routes"));
 app.use("/api/articles", require("./routes/articles.routes"));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client", "build")));
+  app.use(express.static(path.join(__dirname, 'client/build')));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname,'/client/build/index.html'));
   });
 }
 
